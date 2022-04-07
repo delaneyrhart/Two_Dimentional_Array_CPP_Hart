89fe32f7a0ec1f3cf5979b8978cfec8f3638321a
Delaney R. Hart <102171684+delaneyrhart@users.noreply.github.com>
2022-04-07 01:55:21 +0000

    Experimented with program and started extra credit

--- .ccls-cache/@home@runner@TwoDimentionalArrayCPPHart/main.cpp
+++ .ccls-cache/@home@runner@TwoDimentionalArrayCPPHart/main.cpp
@@ -1,4 +1,5 @@
 //Delaney Hart
+//COSC 1437-58002
 //Teacher: Dr. Tyson McMillan
 //Date: 4/4
 //A program to practice working with multi-dimensional arrays
@@ -8,43 +9,41 @@ using namespace std;
 
 /*
 
-Research Input data: go to http://www.weather.com (Links to an external site.)
-
 Pick two cities of your choice. Fill in the temperature for the 7 days of this week of both cities. Fill in this text data table, which will be used in your program.
 
-//City_One = 1, City_Two = 2  Replace City_One and City_Two with the names of your cities
+Clarksville, TN = 1, Boston, MA = 2
 
 //Data Table
 
 //City X, Day X: Temp
 
-City 1, Day 1: TEMP
+City 1, Day 1: 62
 
-City 1, Day 2: TEMP
+City 1, Day 2: 61
 
-City 1, Day 3: TEMP
+City 1, Day 3: 58
 
-City 1, Day 4: TEMP
+City 1, Day 4: 69
 
-City 1, Day 5: TEMP
+City 1, Day 5: 72
 
-City 1, Day 6: TEMP
+City 1, Day 6: 73 
 
-City 1, Day 7: TEMP
+City 1, Day 7: 75
 
-City 2, Day 1: TEMP
+Boston, Day 1: 46
 
-City 2, Day 2: TEMP
+City 2, Day 2: 48
 
-City 2, Day 3: TEMP
+City 2, Day 3: 62
 
-City 2, Day 4: TEMP
+City 2, Day 4: 60
 
-City 2, Day 5: TEMP
+City 2, Day 5: 55
 
-City 2, Day 6: TEMP
+City 2, Day 6: 63
 
-City 2, Day 7: TEMP
+City 2, Day 7: 71
 
 */
 
@@ -67,9 +66,9 @@ int main()
 
     //note for every dimension of the array you need a for loop 2 dimensions = 2 for loops
 
-    for (int i = 0; i < CITY; ++i)
+    for (int i = 0; i < CITY; ++i)//loops 2 times
     {
-        for(int j = 0; j < WEEK; ++j)
+        for(int j = 0; j < WEEK; ++j)//loops 7 times
         {
             cout << "City " << i + 1 << ", Day " << j + 1 << " : ";
             cin >> temperature[i][j];
@@ -80,7 +79,7 @@ int main()
 
     // Accessing the values from the temperature array
 
-    for (int i = 0; i < CITY; ++i)
+    for (int i = 0; i < CITY; ++i)//the same except outputs
     {
         for(int j = 0; j < WEEK; ++j)
         {
@@ -88,14 +87,15 @@ int main()
         }
     }
 
-   /*For up to 5 Points Extra Credit
+  //Create a one dimensional array, cityOne[7] to store the 7 temperatures of city 1, assume that value 0 represents day 1, 1 represents day 2....
+  int count;
+  const int TEMP = 7;
+  int cityOne[TEMP] = {62, 61, 58, 69, 72, 73, 75};
 
-      Expand this program:
+//  Create a on dimensional array, cityTwo[7] to store the 7 temperatures of city 2 , assume that value 0 represents day 1, 1 represents day 2....
+  int cityTwo[TEMP] = {46, 48, 62, 60, 55, 63, 71};
 
-      1. Create a one dimensional array, cityOne[7] to store the 7 temperatures of city 1, assume that value 0 represents day 1, 1 represents day 2....
-
-      2. Create a on dimensional array, cityTwo[7] to store the 7 temperatures of city 2 , assume that value 0 represents day 1, 1 represents day 2....
-
+  /*
       3. Using a for loop (or nested for loop), feed the data of CityOne[] and CityTwo[] into temperature[i][j], where i is index of the City Number, and j is the index of the temp for the day.
 
       4. print the results of temperature[i][j]; to the screen.
